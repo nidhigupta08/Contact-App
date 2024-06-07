1a43c2978f1b499b4690e02333f893e8293a587c
Nidhi <guptanidhi8000@gmail.com>
2024-06-07 23:31:14 +0530

    Add state and form handling to AddContact component

--- src/components/AddContacts.js
+++ src/components/AddContacts.js
@@ -1,17 +1,36 @@
 import React from "react";
 
+//using states in the class components.
 //Class component
 class AddContact extends React.Component{
+    state={
+        name:"",
+        email:""
+    };
+
+    add=(e)=> {
+    e.preventDefault();
+    if(this.state.name==="" && this.state.email==""){
+        alert("All the fields are mendatory!");
+        return;
+    }
+    this.props.addContactHandler(this.state);
+    // console.log(this.state);
+    }
     render(){
         return(
             <div className="ui main">
                 <h2>Add Contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
-                        <input type="text" name="name" placeholder="Name"/>       
+                        <input type="text" name="name" value={this.state.name} placeholder="Name" onChange={(e)=> this.setState({name:e.target.value})}/>       
+                                      
+                    </div>
+                    <div className="field">
                         <label>Email</label>
-                        <input type="email" name="email" placeholder="Email"/>               
+                        <input type="text" name="email" value={this.state.email}  placeholder="Email" onChange={(e)=> this.setState({email:e.target.value})}/>        
+                                      
                     </div>
                     <button className="ui button blue">Add Contact</button>
                 </form>
