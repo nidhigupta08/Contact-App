dc698f616d088c0f7224bac55a7d77d43b2b9486
Nidhi <guptanidhi8000@gmail.com>
2024-06-07 23:33:11 +0530

    Use useState hook for managing contacts in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Header from './Header'; 
 import AddContact from './AddContacts';
 import ContactList from './ContactList';
 function App() {
+  
 //rendering list in react .. created a list using array
-  const contacts=[
-    {
-      id:"1",
-      name:"Nidhi",
-      email:"guptanidhs@gmail.com"
-    },
-    {
-      id:"2",
-      name:"suman",
-      email:"suman@gmail.com"
-    }
-  ];
+  // const contacts=[
+  //   {
+  //     id:"1",
+  //     name:"Nidhi",
+  //     email:"guptanidhs@gmail.com"
+  //   },
+  //   {
+  //     id:"2",
+  //     name:"suman",
+  //     email:"suman@gmail.com"
+  //   }
+  // ];
+
+  //using the "usestate react hook"
+  //if we have a functional component thn we make use of react hook""
+  const [contacts,setContacts]=useState([]) //empty array 
+const  addContactHandler=(contact)=>{
+ console.log(contact);
+};
   return (
     <div className='ui container'> 
       <Header/>
-       <AddContact/>
+       <AddContact addContactHandler={addContactHandler}/>
        {/* Props- pass data from parent to child   & contacts as a property and contacts as array */}
      <ContactList contacts={contacts}/> 
     </div>
