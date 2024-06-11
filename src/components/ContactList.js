d9dd7e413f7548be57f37acfe982a51d1f319257
Nidhi <guptanidhi8000@gmail.com>
2024-06-11 11:49:53 +0530

    Add contact deletion functionality to ContactList
    - Added deleteContactHandler to pass contact ID for deletion.
    - Updated ContactCard component to include clickHandler prop.

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,10 +3,13 @@ import ContactCard from "./ContactCard";
 
 const ContactList =(props)=>{
  
+    const deleteContactHandler=(id)=>{
+        props.getContactId(id);
+    };
     // created a function 
     const renderContactList=props.contacts.map((contact) =>{
         return(
-           <ContactCard contact={contact}/>
+           <ContactCard contact={contact} clickHandler={deleteContactHandler} key={contact.id}/>
         );
     });
     return(
